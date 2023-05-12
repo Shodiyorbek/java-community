a293868f867c8e9140eb6a9c7e269c26908447c4
Shodiyorbek <shodiyorbektolqinov7777@gmail.com>
2023-05-12 12:32:09 +0500

    fronted login register dashbord is done!!!!!

--- src/admin/article/AddCatigory.js
+++ src/admin/article/AddCatigory.js
@@ -1,67 +1,66 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import '../../Style.css'
-import {  Modal } from "antd";
+import "../../Style.css";
+import { Modal } from "antd";
 
-
-
-const AddCatigory = () => {
-    const [catigory, setCatigory] = useState({});
-    const [catigories, setCatigories] =useState([])
+const AddCategory = () => {
+    const [category, setCategory] = useState({});
+    const [categories, setCategories] = useState([]);
     const [error, setError] = useState("");
-    const string = localStorage.getItem('token');
+    const string = localStorage.getItem("token");
     const [visible, setVisible] = useState(false);
+    const [expandedCategories, setExpandedCategories] = useState([]);
     const [id, setId]=useState({})
 
-
-
-
     const handleInputChange = (e)=>{
-        console.log(catigory)
-        setCatigory({
+        console.log(category)
+        setCategory({
                 parentId:id.id,
                 name:e.target.value
             }
         )
 
     }
-    const CategoryList = ({ categories })=> {
 
-        return categories.map((category) => getCategoryContent({category: category, parentCategoryId:null  }));
-    };
-
-    const getCategoryContent=({ category,parentCategoryId })=>{
-        if(category !== undefined) {
-            return (
-                <div>
-                <div className={"paragraph"}  >
-                    <div>{category.name}</div>
-                    <div className={"add-btn btn btn-outline-primary"} onClick={()=>showModal(category.id, parentCategoryId)} >Add child catigory</div>
-                </div>
-                    <div className={"main"}>
-                    {category.childCategoryResponseList?.length > 0 && (
-                        category.childCategoryResponseList.map(child => (
-                            <React.Fragment key={child.id}>
-                                {getCategoryContent({ category: child, parentCategoryId:category.id })}
-                            </React.Fragment>
-                        ))
-                    )}
-
-                    {category.articleResponseList?.length > 0&& (
-                        category.articleResponseList.map(article => (
-                            <React.Fragment key={article.id}>
-                                {getArticleContent({ article:article, parentCategoryId:category.id})}
-                            </React.Fragment>
-                        ))
-                    )}
+    const CategoryList = ({ categories, parentCategoryId }) => {
+        return categories.map((category) => (
+            <React.Fragment key={category.id}>
+                <div className={"paragraph"}>
+                    <div
+                        onClick={() => handleCategoryClick(category.id)}
+                        style={{ cursor: "pointer" }}
+                    >
+                        {category.name}
+                    </div>
+                    <div
+                        className={"add-btn btn btn-outline-primary"}
+                        onClick={() => showModal(category.id, parentCategoryId)}
+                    >
+                        Add child category
                     </div>
                 </div>
-            )
-        }
-        return <></>;
+                {expandedCategories.includes(category.id) && (
+                    <div className={"main"}>
+                        <CategoryList
+                            categories={category.childCategoryResponseList || []}
+                            parentCategoryId={category.id}
+                        />
+                        {category.articleResponseList?.length > 0 &&
+                            category.articleResponseList.map((article) => (
+                                <React.Fragment key={article.id}>
+                                    {getArticleContent({
+                                        article: article,
+                                        parentCategoryId: category.id,
+                                    })}
+                                </React.Fragment>
+                            ))}
+                    </div>
+                )}
+            </React.Fragment>
+        ));
     };
 
-    const getArticleContent =({ article, parentCategoryId })=>{
+    const getArticleContent = ({ article, parentCategoryId }) => {
         return (
             <div>
                 <h5>{article.name}</h5>
@@ -69,30 +68,44 @@ const AddCatigory = () => {
         );
     };
 
+    const handleCategoryClick = (categoryId) => {
+        setExpandedCategories((prevExpandedCategories) => {
+            if (prevExpandedCategories.includes(categoryId)) {
+                // category is currently expanded, so we need to collapse it
+                return prevExpandedCategories.filter(
+                    (expandedCategoryId) => expandedCategoryId !== categoryId
+                );
+            } else {
+                // category is currently collapsed, so we need to expand it
+                return [...prevExpandedCategories, categoryId];
+            }
+        });
+    };
 
-    useState(()=>{
-        axios.get("http://localhost:8080/category",
-            {
+    useEffect(() => {
+        axios
+            .get("http://localhost:8080/category", {
                 headers: {
                     Authorization: `Bearer ${JSON.parse(string)}`,
-                    'Content-Type': 'application/json'
-                }
+                    "Content-Type": "application/json",
+                },
             })
             .then((response) => {
-                setCatigories(response.data)
-
+                setCategories(response.data);
             })
             .catch((error) => {
                 setError("Failed to register.");
-            })
-    })
+            });
+    }, []);
+
+
     const onOkItSelf = (e) => {
         e.preventDefault();
         console.log(string)
 
         const token = JSON.parse(string);
 
-        axios.post("http://localhost:8080/category", catigory,
+        axios.post("http://localhost:8080/category", category,
             {
 
                 headers: {
@@ -127,23 +140,24 @@ const AddCatigory = () => {
             id:id,
             parentId:parentId
         })
-    };
-    return (
-        <>
-            <button className={"btn btn-outline-primary"} onClick={ ()=>showModal(null,null)} type={"button"}>Add super catigory</button>
-            <Modal title="Basic Modal" open={visible} onOk={onOkItSelf} onCancel={handleCancel}>
-                <div className="form-group">
-                    <label htmlFor="">Catigory name</label>
-                    <input  className="form-control" onChange={handleInputChange} name="file" />
-                </div>
-            </Modal>
-            <div  className={"container-main"}>
-                <CategoryList categories={catigories} />
 
+    }
+return (
+    <div className={"alls"}>
+        <button className={"btn btn-outline-primary"} onClick={ ()=>showModal(null,null)} type={"button"}>Add super catigory</button>
+        <Modal title="Basic Modal" open={visible} onOk={onOkItSelf} onCancel={handleCancel}>
+            <div className="form-group">
+                <label htmlFor="">Catigory name</label>
+                <input  className="form-control" onChange={handleInputChange} name="file" />
             </div>
-        </>
+        </Modal>
+        <div  className={"container-main"}>
+            <CategoryList categories={categories} />
 
-    );
-};
+        </div>
+    </div>
 
-export default AddCatigory;
+)};
+
+
+export default  AddCategory;
